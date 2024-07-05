7cfc5ee222f12feadf31707386178a95e53f94a0
Abhay <96972917+Abhay1520@users.noreply.github.com>
2024-07-05 23:52:04 +0530

    Add files via upload

---
+++ client/src/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "./globals.css";
+
+
+const IndexView = () => {
+    return(
+        <div className='w-full flex item-center justify-center min-h-[80vh'>
+            <div className='w-1/3 border min-h-20 p-5 flex flex-col gay-y-4'>
+            <h1 className="text-white text-lg"> Name: Abhay </h1>
+            <h1 className="text-white text-lg"> Email: Abhay </h1>
+            <h1 className="text-white text-lg"> 
+
+            <button className='px-6 py-2 rounded-md bg-green-400'>Logout</button> </h1>
+
+            </div>
+        </div>
+    )
+}
